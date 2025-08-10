cce9d4d9fec46c5676e795417e24e405ccdeafa2
ramywael <ramybakry9@gmail.com>
2025-08-10 11:45:35 +0300

    GetAllFeaturesQuery.cs

---
+++ Features/Feature/GetAllFeature/Endpoint/GetAllFeaturesController.cs
@@ -0,0 +1,36 @@
+﻿using HRSystem.Common;
+using HRSystem.Common.Enums;
+using HRSystem.Common.Views;
+using HRSystem.Features.Feature.GetAllFeature.Queries;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRSystem.Features.Feature.GetAllFeature.Endpoint
+{
+
+    public class GetAllFeaturesController : BaseEndPoint<GetAllFeaturesQuery, IEnumerable<GetAllFeaturesResponseViewModel>>
+    {
+        public GetAllFeaturesController(EndPointBaseParameters<GetAllFeaturesQuery> parameters) : base(parameters)
+        {
+        }
+
+        [HttpGet]
+        public async Task<EndPointResponse<IEnumerable<GetAllFeaturesResponseViewModel>>> GetAllFeatures()
+        {
+            var result = await mediator.Send(new GetAllFeaturesQuery());
+
+            if (result?.Data == null)
+                return EndPointResponse<IEnumerable<GetAllFeaturesResponseViewModel>>.Failure(ErrorCodes.Null);
+
+
+            if (!result.Data.Any())
+                return EndPointResponse<IEnumerable<GetAllFeaturesResponseViewModel>>.Failure(ErrorCodes.NotFound);
+
+            var responseViewModel = mapper.Map<IEnumerable<GetAllFeaturesResponseViewModel>>(result.Data);
+
+            return EndPointResponse<IEnumerable<GetAllFeaturesResponseViewModel>>.Success(responseViewModel);
+
+
+        }
+    }
+}
---
+++ Features/Feature/GetAllFeature/Mapping Profile/GetAllFeaturesProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using HRSystem.Features.Feature.GetAllFeature.DTOs;
+
+namespace HRSystem.Features.Feature.GetAllFeature.Mapping_Profile
+{
+    public class GetAllFeaturesProfile : Profile
+    {
+        public GetAllFeaturesProfile() 
+        {
+            CreateMap<Models.Feature, GetAllFeaturesResponseDTO>();
+            CreateMap<GetAllFeaturesResponseDTO, GetAllFeaturesResponseViewModel>();
+        }
+    }
+}
