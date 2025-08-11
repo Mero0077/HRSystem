7f1055f0939e1c2ca93e7cdd8021ccf2215979bb
Mero0077 <omartalaatsaad2017@gmail.com>
2025-08-11 17:46:31 +0300

    UpdateLogin

---
+++ Features/Common/UserRole/GetUserWithRole/DTO/GetUserWithRoleDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace HRSystem.Features.Common.UserRole.GetUserWithRole.DTO
+{
+    public class GetUserWithRoleDTO
+    {
+        public Guid UserId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public List<Guid> RoleIds { get; set; }
+
+    }
+}
---
+++ Features/Common/UserRole/GetUserWithRole/UserWithRoleResponseVM.cs
@@ -0,0 +1,10 @@
+﻿namespace HRSystem.Features.Common.UserRole.GetUserWithRole
+{
+    public class UserWithRoleResponseVM
+    {
+        public Guid UserId { get; set; }
+        public string UserName { get; set; }
+        public List<Guid> RoleIds { get; set; }
+
+    }
+}
